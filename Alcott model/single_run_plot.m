9e9f618a8458e8b47058a7ec575048f229a53cad
Benjamin Mills <bjwmills@gmail.com>
2024-06-24 15:46:51 +0100

    Add files via upload

--- Alcott model/single_run_plot.m
+++ Alcott model/single_run_plot.m
@@ -1,41 +1,41 @@
 figure
 
-subplot(5,3,1)
+subplot(4,4,1)
 plot(state.time/10e5,state.Aiso)
 xlabel('Time (Ma)')
 ylabel('δ^{13}C (‰)')
 hold on
 box on
 %%
-subplot(5,3,2)
+subplot(4,4,2)
 plot(state.time/10e5,state.CO2atm/10e-7)
 xlabel('Time (Ma)')
 ylabel('CO_{2} (ppm)')
 hold on
 box on
 %%
-subplot(5,3,3)
+subplot(4,4,3)
 plot(state.time/10e5,state.O2_A/present.O2_A)
 xlabel('Time (Ma)')
 ylabel('O_{2} (atmosphere) (PAL)')
 hold on
 box on
 %%
-subplot(5,3,4)
+subplot(4,4,4)
 plot(state.time/10e5,state.O2_DP/present.O2_A)
 xlabel('Time (Ma)')
 ylabel('O_{2} (Deep ocean) (PAL)')
 hold on
 box on
 %%
-subplot(5,3,5)
+subplot(4,4,5)
 plot(state.time/10e5,state.GAST-273.15)
 xlabel('Time (Ma)')
 ylabel('Global average surface temperature (℃)')
 hold on
 box on
 %%
-subplot(5,3,6)
+subplot(4,4,6)
 plot(state.time/10e5,state.Prox_Preac_Burial)
 hold on
 plot(state.time/10e5,state.Dist_Preac_Burial)
@@ -45,7 +45,7 @@ hold on
 box on
 xlabel('Time (Ma)'),ylabel('P burial (mol yr^{-1})'),legend('Proximal shelf','Distal shelf','Deep ocean')
 %%%
-subplot(5,3,7)
+subplot(4,4,7)
 plot(state.time/10e5,state.CP_Prox)
 hold on
 plot(state.time/10e5,state.CP_Dist)
@@ -57,45 +57,48 @@ hold on
 box on
 xlabel('Time (Ma)'),ylabel('C:P ratio'),legend('Proximal shelf','Distal shelf','Deep ocean','Anoxic')
 %%
-subplot(5,3,8)
-plot(state.time/10e5,state.Total_POC_Burial)
-hold on
+subplot(4,4,8)
 plot(state.time/10e5,state.Fmocb)
 hold on
 plot(state.time/10e5,state.Atmos_Weather)
 hold on
 box on
-xlabel('Time (Ma)'),ylabel('x'),legend('Total POC Burial','Total organic carbon burial','Atmosphere Weather')
+xlabel('Time (Ma)'),ylabel('x'),legend('Total organic carbon burial','Oxidative Weathering')
 %%%
-subplot(5,3,9)
-plot(state.time/10e5,state.SRP_DP)
+subplot(4,4,9)
+semilogy(state.time/10e5,state.SRP_Pconc)
+hold on
+semilogy(state.time/10e5,state.SRP_Dconc)
+semilogy(state.time/10e5,state.SRP_Sconc)
+semilogy(state.time/10e5,state.SRP_DPconc)
 xlabel('Time (Ma)')
-ylabel('Soluble Reactive Phosphorous (Deep ocean)')
+ylabel('SRP')
+legend('SRP (Prox)','SRP (dist)','SRP (surf)','SRP (deep)')
 hold on
 box on
 %%
-subplot(5,3,10)
+subplot(4,4,10)
 plot(state.time/10e5,state.River_SRP)
 xlabel('Time (Ma)')
 ylabel('Riverine P input')
 hold on
 box on
 %%
-subplot(5,3,11)
+subplot(4,4,11)
 plot(state.time/10e5,state.A)
 xlabel('Time (Ma)')
 ylabel('inorganic carbon reservoir')
 hold on
 box on
 %%
-subplot(5,3,12)
+subplot(4,4,12)
 plot(state.time/10e5,state.D)
 xlabel('Time (Ma)')
 ylabel('Degassing rate (relative to present)')
 hold on
 box on
 %%
-subplot(5,3,13)
+subplot(4,4,13)
 plot(state.time/10e5,state.fbiota)
 hold on
 plot(state.time/10e5,state.EXPOSED)
@@ -106,7 +109,7 @@ box on
 xlabel('Time (Ma)'),ylabel(''),legend('Biological weathering enhancement','Exposed landmass','Crustal carbon reservoir')
 %%
 
-subplot(5,3,15)
+subplot(4,4,14)
 plot(state.time/10e5,state.fanoxicprox)
 hold on
 plot(state.time/10e5,state.fanoxicdist)
@@ -114,6 +117,34 @@ hold on
 box on
 xlabel('Time (Ma)'),ylabel('f'),legend('proximal anoxic','distal anoxic')
 %%
+
+%%%
+subplot(4,4,15)
+semilogy(state.time/10e5,state.NO3_Pconc)
+hold on
+semilogy(state.time/10e5,state.NO3_Dconc)
+semilogy(state.time/10e5,state.NO3_Sconc)
+semilogy(state.time/10e5,state.NO3_DPconc)
+xlabel('Time (Ma)')
+ylabel('NO3')
+legend('NO3 (Prox)','NO3 (dist)','NO3 (surf)','NO3 (deep)')
+hold on
+box on
+
+%%%
+subplot(4,4,16)
+semilogy(state.time/10e5,state.NH4_Pconc)
+hold on
+semilogy(state.time/10e5,state.NH4_Dconc)
+semilogy(state.time/10e5,state.NH4_Sconc)
+semilogy(state.time/10e5,state.NH4_DPconc)
+xlabel('Time (Ma)')
+ylabel('NH4')
+legend('NH4 (Prox)','NH4 (dist)','NH4 (surf)','NH4 (deep)')
+hold on
+box on
+
+
 
 
 %subplot(5,6,25)
